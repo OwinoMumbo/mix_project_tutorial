fc7e94e4653f4687a4aff4fcc70dc58217f20868
OwinoMumbo <sowino2@gmail.com>
2023-07-15 18:12:09 +0200

    project setup 1

--- tutorials/lib/tutorials.ex
+++ tutorials/lib/tutorials.ex
@@ -1,17 +1,4 @@
 defmodule Tutorials do
-  @moduledoc """
-  Documentation for `Tutorials`.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Tutorials.hello()
-      :world
-
-  """
   def hello do
     :world
   end
