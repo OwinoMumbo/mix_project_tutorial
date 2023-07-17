9996e0a0a7b06c105fe36dac4cc655aa5977ea41
OwinoMumbo <sowino2@gmail.com>
2023-07-17 02:44:22 +0200

    recursion by factorial

--- tutorials/lib/recursion/print_digits.ex
+++ tutorials/lib/recursion/print_digits.ex
@@ -3,7 +3,7 @@ defmodule Tutorials.Recursion.PrintDigits do
   def upto(0), do: 0
 
   def upto(num) do
-    IO.puts(num)# 3,2,1,0
+    IO.puts(num)# 3,2,1,0 put this as line 7 to make ascending order/ tail recursion
     upto(num - 1)# reducing number
   end
 end
